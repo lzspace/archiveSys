a9fed6b999134dad35b13c0c048fd53118ca06af
lzspace <106171510+lzspace@users.noreply.github.com>
2023-02-21 17:54:16 +0100

    connect with database

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-hello world
+$db = new PDO("mysql:host=127.0.0.1;dbname=test;charset=utf8mb4", 'root', '');
+
+$select = $db->query(file_get_contents('sql/fetch_all_colors.sql'));
+while ($row = $select->fetch(PDO::FETCH_ASSOC)) {
+    echo $row['color'].'<br>';
+}
