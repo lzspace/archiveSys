b0800e2187ebe8ec6c175dcef5ee947b48a680b8
tili7 <118210814+tili7@users.noreply.github.com>
2023-08-30 23:26:32 +0200

    added test for connecting front and backen

--- mysql/server.php
+++ mysql/server.php
@@ -9,4 +9,31 @@ $db_name = "archivsys";
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $connection = mysqli_connect($host, $user, $pass, $db_name);
 
+
+
+//get the form data
+$name = $_POST['name'];
+$description = $_POST['description'];
+$keywords = $_POST['keywords'];
+
+//insert the new entry into the database
+$sql = "INSERT INTO binders (name, description, keywords) VALUES ('$name', '$description', '$keywords')";
+$query = $db->query($sql);
+
+//check if the query was successful
+if ($query) {
+    //the query was successful
+    echo "The new entry was successfully inserted into the database.";
+} else {
+    //the query failed
+    echo "The new entry could not be inserted into the database.";
+}
+
 ?>
+
+
+
+
+
+
+
