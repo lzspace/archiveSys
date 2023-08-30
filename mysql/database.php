e2654d720ea0eeb32867e7be0b9da778a4cc51ab
tili7 <118210814+tili7@users.noreply.github.com>
2023-08-31 00:06:44 +0200

    small changes

--- mysql/database.php
+++ mysql/database.php
@@ -1,9 +1,15 @@
 <?
 
 
-//include the database connection file
-include 'index.php';
+//database variables
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db_name = "archivsys";
 
+//connection
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$connection = mysqli_connect($host, $user, $pass, $db_name);
 //get the form data
 $name = $_POST['name'];
 $description = $_POST['description'];
